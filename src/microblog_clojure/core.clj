02a39bdf7f8e64aaa572de0dacfb7160d9539369
Calvin Webster <calweb@gmail.com>
2016-03-24 11:50:14 -0400

    persist in edn

--- src/microblog_clojure/core.clj
+++ src/microblog_clojure/core.clj
@@ -7,6 +7,11 @@
   (:gen-class))
 
 (defonce messages (atom []))
+(defonce server (atom nil))
+
+(add-watch messages :save-to-disk
+  (fn [_ _ _ _]
+    (spit "messages.edn" (pr-str @messages))))
 
 (c/defroutes app
   (c/GET "/" request
@@ -19,16 +24,20 @@
                (map (fn [message]
                       [:li message])
                  @messages)]]]))
+  
   (c/POST "/add-message" request
     (let [message (get (:params request) "message")]
       (swap! messages conj message)
       (println (str @messages))
-      (r/redirect "/"))))
-              
 
-(defonce server (atom nil))
+      (r/redirect "/"))))
 
 (defn -main []
+  (try
+    (let [messages-str (slurp "messages.edn")
+          messages-vec (read-string messages-str)]
+      (reset! messages messages-vec))
+    (catch Exception _))
   (when @server
     (.stop @server))
   (reset! server (j/run-jetty (p/wrap-params app) {:port 3000 :join? false})))
